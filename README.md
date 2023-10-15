dde92d10433c454dafad43fdd5b709aecd529f3d
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2023-10-16 02:47:32 +0300

    MAKE IT REAL

---
+++ README.md
@@ -0,0 +1,3 @@
+This app is for the shortage of calculators on the market.
+
+Worry no more, daddy is here.
--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-let firstNumber = "", lastNumber = "", operator = "";
+const display = document.getElementById("display");
+let firstNumber = "", lastNumber = "", operator = "", sum;
 
 function add(a, b) {
-    // return a + b;
-    console.log(a + b);
+    sum = a + b;
+    display.innerText = (sum);
+    firstNumber = sum;
+    lastNumber = "";
 }
 
 function subtract(a, b) {
-    // return a - b;
-    console.log(a - b);
+    sum = a - b;
+    display.innerText = (sum);
+    firstNumber = sum;
+    lastNumber = "";
 }
 
 function multiply(a, b) {
-    // return a * b;
-    console.log(a * b);
+    sum = a * b;
+    display.innerText = (sum);
+    firstNumber = sum;
+    lastNumber = "";
 }
 
 function divide(a, b) {
-    // return a / b;
-    console.log(a / b);
+    if (b === 0) { display.innerText = ("Can't divide by 0") }
+    else {
+        sum = a / b;
+        display.innerText = (sum);
+        firstNumber = sum;
+        lastNumber = "";
+    }
 }
+
 
 function operate(firstNum, op, lastNum) {
     switch (op) {
@@ -28,37 +41,57 @@ function operate(firstNum, op, lastNum) {
         case "-":
             subtract(firstNum, lastNum);
             break;
-        case "*":
+        case "x":
             multiply(firstNum, lastNum);
             break;
         case "/":
             divide(firstNum, lastNum);
             break;
-        default: // TODO: update the display saying "invalid"
+        default:
+            resetAll();
+            display.innerText = "invalid operator!";
             break;
     }
 }
 
 function determineOperator(op) {
-    operator = op;
-    console.log(operator);
+    if (op === "-") {
+        if (firstNumber === "") {
+            firstNumber += op;
+            display.innerText += op;
+        }
+        else if (operator !== "" && lastNumber === "") {
+            lastNumber += op;
+            display.innerText += op;
+        } else {
+            operator = op;
+            display.innerText += op;
+        }
+    }
+    else if (firstNumber !== "" && firstNumber !== "-" && operator.length == 0) {
+        operator = op;
+        display.innerText += op;
+    }
+
 }
 
 function setNumber(num) {
     if (operator == "") {
         firstNumber += num;
+        display.innerText += num;
     }
     else {
+        display.innerText += num;
         lastNumber += num;
     }
-    console.log(firstNumber);
-    console.log(lastNumber);
 }
 
 function equalsTo() {
     operate(Number(firstNumber), operator, Number(lastNumber));
+    operator = "";
+    console.log("first num: " + firstNumber + " last num: " + lastNumber + " sum: " + sum)
 }
 
 function resetAll() {
-    firstNumber = ""; lastNumber = ""; operator = "";
+    firstNumber = ""; lastNumber = ""; operator = ""; display.innerText = "", sum = 0;
 }
